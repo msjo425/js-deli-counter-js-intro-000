5ffc95d4317444e0e88ed0798239ed7e65a0b0b0
matt joseloff <msjo425@gmail.com>
2020-05-08 18:50:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(line,name){
 
 function nowServing(line){
   if (line.length > 0) {
-  return `Currently serving ${line[0]}`
+  return `Currently serving ${line[0]}.`
   line.shift()
 }
   else {
