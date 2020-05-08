ccaf758b4b3533a1eca038f7e7f3377cc065e1ba
matt joseloff <msjo425@gmail.com>
2020-05-08 19:17:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,8 +16,7 @@ function nowServing(line){
 function currentLine (line){
   var cue = ""
   if (line.length > 0){
-  let i = 0
-  for (i < line.length) {
+  for (i =0, i < line.length) {
     cue = cue + line[i]
 }
   return `The line is currently ${cue}`
